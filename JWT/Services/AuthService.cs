c4d921b4fce6af760bfb7d0defbfb5a794dff13f
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-03-04 17:34:31 +0200

    UpdateCreateJwtToken

--- JWT/Services/AuthService.cs
+++ JWT/Services/AuthService.cs
@@ -1,16 +1,27 @@
 ﻿using JWT.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System;
 using System.Threading.Tasks;
+using System.Linq;
+using JWT.Helpers;
+using Microsoft.Extensions.Options;
 
 namespace JWT.Services
 {
     public class AuthService : IAuthService
     {
         private readonly UserManager<ApplicationUser> userManager;
+        private readonly JWTT jwt;
 
-        public AuthService(UserManager<ApplicationUser> userManager)
+        public AuthService(UserManager<ApplicationUser> userManager , IOptions<JWTT> jwt)
         {
             this.userManager = userManager;
+            jwt = jwt;
         }
         public async Task<AuthModel> RegisterAsync(RegisterModel model)
         {
@@ -18,7 +29,7 @@ namespace JWT.Services
                 return new AuthModel { Message = " Email already Registtred"};
             }
 
-            if (userManager.FindByEmailAsync(model.Username) is not null)
+            if (userManager.FindByNameAsync(model.Username) is not null)
             {
                 return new AuthModel { Message = "UserName is already Existed" };
             }
@@ -46,6 +57,38 @@ namespace JWT.Services
             }
 
             await userManager.AddToRoleAsync(user, "User");
+        }
+
+        private async Task<JwtSecurityToken> CreateJwtToken(ApplicationUser user)
+        {
+            var userClaims = await userManager.GetClaimsAsync(user);
+            var roles = await userManager.GetRolesAsync(user);
+            var roleClaims = new List<Claim>();
+
+            foreach (var role in roles)
+                roleClaims.Add(new Claim("roles", role));
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id)
+            }
+            .Union(userClaims)
+            .Union(roleClaims);
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: jwt.Issuer,
+                audience: jwt.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddDays(jwt.DurationInDays),
+                signingCredentials: signingCredentials);
+
+            return jwtSecurityToken;
         }
     }
 }
